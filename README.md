ed3fd700dbbbc13e1cfe3cf2c732ac2d8e29a112
Diego Managlia <diego.managlia@gmail.com>
2024-08-20 13:46:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Costs - React Project
+
+This project was made with React in front-end and it is part of a React trainning.
---
+++ src/App.js
@@ -0,0 +1,37 @@
+// import './App.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Layout
+import Container from "./components/layout/Container"
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+
+// Pages
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Container customClass="min-height">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/company" element={<Company />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/newproject" element={<NewProject />} />
+                    <Route path="/project/:id" element={<Project />} />
+                </Routes>
+            </Container>
+            <Footer />
+        </Router>
+    );
+}
+
+export default App;
